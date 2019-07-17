9bd76ffd07208e31c34791eed846f483c32753a5
Prabhanu <prabhanu96@gmail.com>
2019-07-17 19:13:43 +0530

    pass routing parameter from models to model-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ModelsComponent } from './models/models.component';
 import { DataComponent } from './data/data.component';
 
 const routes: Routes = [
-  {path:'model-create',component:ModelCreateComponent},
+  {path:'model-create/:modelName',component:ModelCreateComponent},
   {path:'',component:ModelsComponent},
   {path:'model/data/:docId',component:DataComponent,data:[]}
 ];
